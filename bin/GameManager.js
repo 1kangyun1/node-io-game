ac6ac7812a619243f9f81eb45fd63ad66629c8f4
Brian Kang <1kangyun1@gmail/com>
2021-01-20 16:10:56 -0500

    decoupled responsibility from player and gameManager

--- bin/GameManager.js
+++ bin/GameManager.js
@@ -32,8 +32,13 @@ class GameManager {
     const index = this.players.findIndex(player => player.id === id);
 
     if (this.players[index]) {
-      this.players[index].setDirection(dir);
+      this.players[index].direction = dir;
     }
+  }
+
+  movePlayer(player, dt){
+    player.pos.x += dt * player.direction.dirX * player.speed;
+    player.pos.y += dt * player.direction.dirY * player.speed;
   }
 
   update(){
@@ -41,7 +46,7 @@ class GameManager {
     const dt = (now - this.lastUpdateTime) / 1000;
     this.lastUpdateTime = now;
 
-    this.players.forEach(player => player.move(dt));
+    this.players.forEach(player => this.movePlayer(player, dt));
 
     this.io.emit('gameUpdate', this.players);
   }
--- bin/Player.js
+++ bin/Player.js
@@ -5,16 +5,8 @@ class Player{
     this.id = id;
     this.username = username;
     this.pos = {x,y};
+    this.speed = 100;
     this.direction = { dirX:0, dirY:0 };
-  }
-
-  move(dt){
-    this.pos.x += dt * this.direction.dirX * 100;
-    this.pos.y += dt * this.direction.dirY * 100;
-  }
-
-  setDirection(dir){
-    this.direction = dir;
   }
 }
 
