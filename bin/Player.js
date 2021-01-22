71ca813317516c28aebbaa130ef2c6952f5de65c
ditto000 <richard.0125@hotmail.com>
2021-01-22 00:17:49 -0500

    changed spacing and speed

--- bin/Player.js
+++ bin/Player.js
@@ -1,12 +1,10 @@
-
-
-class Player{
-  constructor(id, username, x, y){
+class Player {
+  constructor(id, username, x, y) {
     this.id = id;
     this.username = username;
-    this.pos = {x,y};
-    this.speed = 100;
-    this.direction = { dirX:0, dirY:0 };
+    this.pos = { x, y };
+    this.speed = 200;
+    this.direction = { dirX: 0, dirY: 0 };
   }
 }
 
--- bin/SocketHandler.js
+++ bin/SocketHandler.js
@@ -6,18 +6,21 @@ function socketHandler(io, socket) {
   const Game = new GameManager(io);
 
   //receives json file containing username string
-  socket.on('join', ( { username } ) => {
-    Game.addPlayer(socket.id, 'test')});
+  socket.on('join', ({ username }) => {
+    Game.addPlayer(socket.id, 'test');
+  });
 
   /** receives dir json file containing x and y direction
    *  directions can be positive, 0, negative
    **/
-  socket.on('movement', ( dir={ dirX, dirY } ) => {
-    Game.handleMovement(socket.id, dir)});
+  socket.on('movement', (dir = { dirX, dirY }) => {
+    Game.handleMovement(socket.id, dir);
+  });
 
   socket.on('disconnect', () => {
     console.log('user has disconnected');
-    Game.removePlayer(socket.id)});
+    Game.removePlayer(socket.id);
+  });
 }
 
 module.exports = socketHandler;
